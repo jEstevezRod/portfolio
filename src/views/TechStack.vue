bd58dc0bd63810e8147fc2c8e26ae7f0eebee1e4
Jesus Estevez <jesestrod1@gmail.com>
2022-04-05 19:03:24 +0200

    Adding logos

---
+++ src/views/TechStack.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="h-full w-full mt-10 text-left">
+        <div class="stack w-full flex px-96">
+            <div class="row w-full flex justify-center gap-12">
+                <div class="col w-52">
+                    <div class="flex flex-col items-start">
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Languages</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/javascript.svg" alt=""></span> <span class="w-full ml-4">JavaScript</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/php.svg" alt=""></span> <span class="w-full ml-4">PHP</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/python.svg" alt=""></span> <span class="w-full ml-4">Python</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/typescript-icon.svg" alt=""></span> <span class="w-full ml-4">Typescript</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Front-End Frameworks</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/vuetifyjs.svg" alt=""></span> <span class="w-full ml-4">Vuetify</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/quasar.png" alt=""></span> <span class="w-full ml-4">Quasar Framework</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Back-End Frameworks</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/laravel.svg" alt=""></span> <span class="w-full ml-4">Laravel</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/nodejs-icon.svg" alt=""></span> <span class="w-full ml-4">Node.js</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Vue libraries</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/vue.svg" alt=""></span> <span class="w-full ml-4">Vue CLI</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/vue.svg" alt=""></span> <span class="w-full ml-4">Vue Router</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/vue.svg" alt=""></span> <span class="w-full ml-4">Vuex</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Virtual machine Platforms</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/docker-icon.svg" alt=""></span> <span class="w-full ml-4">Docker</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/docker-compose.png" alt=""></span> <span class="w-full ml-4">Docker compose</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Cross-Platform Mobile</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/ionic-icon.svg" alt=""></span> <span class="w-full ml-4">Ionic</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Issue Tracking</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/youtrack.svg" alt=""></span> <span class="w-full ml-4">YouTrack</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/teamwork-icon.svg" alt=""></span> <span class="w-full ml-4">Teamwork</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col w-52">
+                    <div class="flex flex-col items-start">
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Javascript Frameworks</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/vue.svg" alt=""></span> <span class="w-full ml-4">Vue.js</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/angular-icon.svg" alt=""></span> <span class="w-full ml-4">Angular 2+</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/nuxt-icon.svg" alt=""></span> <span class="w-full ml-4">Nuxt.js</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">UI Libraries</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/tailwindcss-icon.svg" alt=""></span> <span class="w-full ml-4">Tailwind</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/element.svg" alt=""></span> <span class="w-full ml-4">ElementUI</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/bootstrap.svg" alt=""></span> <span class="w-full ml-4">Bootstrap</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/bulma.svg" alt=""></span> <span class="w-full ml-4">Bulma</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Web servers</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/nginx.svg" alt=""></span> <span class="w-full ml-4">nginx</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/apache.svg" alt=""></span> <span class="w-full ml-4">Apache HTTP Server</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">JS Charting libraries</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/ECharts.jpg" alt=""></span> <span class="w-full ml-4">ECharts</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/amCharts.jpg" alt=""></span> <span class="w-full ml-4">amCharts</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/Highcharts.png" alt=""></span> <span class="w-full ml-4">Highcharts</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/ApexCharts.jpg" alt=""></span> <span class="w-full ml-4">ApexCharts</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Databases</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/postgresql.svg" alt=""></span> <span class="w-full ml-4">PostgreSQL</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/mysql-icon.svg" alt=""></span> <span class="w-full ml-4">MySQL</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/firebase.svg" alt=""></span> <span class="w-full ml-4">Firebase Firestore</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/mongodb.svg" alt=""></span> <span class="w-full ml-4">MongoDB</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">Message Queue</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/rabbitmq-icon.svg" alt=""></span> <span class="w-full ml-4">RabbitMQ</span></div>
+                            </div>
+                        </div>
+                        <div class="my-6 w-full">
+                            <p class="text-gray-500 text-start font-bold mb-6">IDE</p>
+                            <div class="flex flex-col">
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/phpstorm.svg" alt=""></span> <span class="w-full ml-4">PhpStorm</span></div>
+                                <div class="flex nowrap my-1"><span class="w-6"><img src="../assets/images/logos/visual-studio-code.svg" alt=""></span> <span class="w-full ml-4">Visual Studio Code</span></div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style scoped>
+    .tech-stack-sub-title {
+    }
+</style>
